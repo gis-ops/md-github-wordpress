dabcb2b6e215422a165a7ac521474f9bda1ebea7
nilsnolde <nilsnolde@gmail.com>
2019-01-05 13:37:17 +0300

    start fresh repo

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # md-github-wordpress
-Quick WP plugin to add markdown content from Github to a WP blog; based on https://github.com/ghandic/nbconvert
+Quick WP plugin to add markdown content from Github to a WP blog; based on Andy Challis' https://github.com/ghandic/nbconvert.
+
+## Usage
---
+++ md_github.php
@@ -0,0 +1,83 @@
+<?php
+   /*
+   Plugin Name: md-github
+   Description: A plugin to inject markdown files directly into a post from Github
+   Version: 0.1
+   Author: Nils Nolde
+   Author URI: https://gis-ops.com
+   License: MIT
+   */
+
+function md_github_handler($atts) {
+  //get API path from file URL
+  $json = get_api_path($atts, 'file');
+  $md_output = base64_decode($json['content']);
+  //send back text to replace shortcode in post
+  return $md_output;
+}
+
+function md_github_checkout($atts) {
+  $last_update_htnl = get_github_checkout($atts, 'checkout');
+  return $last_update_htnl
+}
+
+function get_api_path($atts, $method) {
+
+  extract(shortcode_atts(array(
+          'url' => "",
+        ), $atts
+      )
+   );
+
+  $url_list = explode('/', $url);
+  $owner = $url_list[3];
+  $repo = $url_list[4];
+  $branch = $url_list[6];
+  $path = implode("/", array_slice($url_list, 7));
+
+  if ($method === 'file') {
+    $request_url = 'https://api.github.com/repos/'.$owner.'/'.$repo.'/contents/'.$path.'?ref='.$branch.;
+  } else {
+    $request_url = 'https://api.github.com/repos/'.$owner.'/'.$repo.'/commits/'.$branch.'?path='. $path.'&page=1';
+  }
+
+  $context_params = array(
+    'http' => array(
+      'method' => 'GET',
+      'user_agent' => 'Bogus user agent',
+      'timeout' => 1
+    )
+  );
+
+  $res = file_get_contents($request_url, FALSE, stream_context_create($context_params));
+  $json = json_decode($res, true);
+
+  return $json
+}
+
+function get_github_checkout($json) {
+
+  $last_update_date_time = $url['commit']['committer']['date'];
+
+  $checkout_label = '<div class="markdown-github"
+      <div class="markdown-github-labels">
+        <label class="github-link">
+          <a href="'.$url.'" target="_blank">Check it out on github</a>
+          <label class="github-last-update"> Last updated: '.$last_update_date_time.'</label>
+        </label>
+      </div>
+    </div>';
+
+  return $checkout_label
+  }
+
+function get_markdown_content($json) {
+  return
+}
+
+function md_github_enqueue_style() {
+	wp_enqueue_style( 'md-github', plugins_url( '/css/github-markdown.css', __FILE__ ));
+}
+add_action( 'wp_enqueue_scripts', 'md_github_enqueue_style' );
+add_shortcode('checkout_github', "md_github_checkout")
+add_shortcode("md_github", "md_github_handler");
